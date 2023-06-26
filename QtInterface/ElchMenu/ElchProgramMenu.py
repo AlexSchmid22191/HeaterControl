ffa3fe6500e36fd9c63bff2657fbe2c714cb2495
Alex <alex.schmid@gmail.com>
2023-06-26 17:58:05 +0200

    Changes the layout of the elements in the setpoint programmer menu

--- QtInterface/ElchMenu/ElchProgramMenu.py
+++ QtInterface/ElchMenu/ElchProgramMenu.py
@@ -4,12 +4,32 @@ from PySide2.QtWidgets import QWidget, QLabel, QDoubleSpinBox, QRadioButton, QVB
 from Signals import engine_signals
 
 
+
+
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
+import sys
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide2.QtGui import QPalette, QColor
+
+
+
+
+
 class ElchProgramMenu(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Refactor to use gridlayout
-        self.labels = {key: QLabel(text=key, objectName='Header') for key in ['Rate', 'Setpoint', 'Hold']}
+        self.labels = {key: QLabel(text=key) for key in ['Rate', 'Setpoint', 'Hold']}
         self.entries = {segment: {'Rate': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=120),
                                   'Setpoint': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=1200),
                                   'Hold': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=100)
@@ -28,48 +48,43 @@ class ElchProgramMenu(QWidget):
         engine_signals.ramp_segment_started.connect(self.mark_ramp_segment)
         engine_signals.hold_segment_started.connect(self.mark_hold_segment)
 
-        self.buttons = {key: QPushButton(text=key, objectName=key) for key in ['Start', 'Stop']}
-        self.buttons['Start'].clicked.connect(self.start_programm)
+        grid = QGridLayout()
+        grid.setSpacing(5)
+        grid.addWidget(self.labels['Rate'], 0, 1)
+        grid.addWidget(self.labels['Setpoint'], 0, 2)
+        grid.addWidget(self.labels['Hold'], 0, 3)
+
+        for segment, seg_dict in self.entries.items():
+            grid.addWidget(self.radios[segment][0], segment, 0)
+            grid.addWidget(seg_dict['Rate'], segment, 1)
+            grid.addWidget(seg_dict['Setpoint'], segment, 2)
+            grid.addWidget(seg_dict['Hold'], segment, 3)
+            grid.addWidget(self.radios[segment][1], segment, 4)
+
+        self.start_button = QPushButton(text='Start', objectName='Start')
+        self.start_button.setCheckable(True)
+        self.start_button.toggled.connect(self.start_programm)
 
         vbox = QVBoxLayout()
         vbox.setSpacing(0)
         vbox.setContentsMargins(10, 10, 10, 10)
 
-        hbox_header = QHBoxLayout()
-        for label in self.labels.values():
-            hbox_header.addWidget(label)
-            hbox_header.addStretch(1)
-        vbox.addLayout(hbox_header)
+        vbox.addLayout(grid)
         vbox.addSpacing(10)
-
-        for segment, seg_dict in self.entries.items():
-            line_box = QHBoxLayout()
-            line_box.addWidget(self.radios[segment][0])
-            line_box.addSpacing(5)
-            line_box.addWidget(seg_dict.get('Rate'))
-            line_box.addSpacing(5)
-            line_box.addWidget(seg_dict.get('Setpoint'))
-            line_box.addSpacing(5)
-            line_box.addWidget(seg_dict.get('Hold'))
-            line_box.addSpacing(5)
-            line_box.addWidget(self.radios[segment][1])
-            vbox.addLayout(line_box)
-            vbox.addSpacing(10)
-
-        vbox.addSpacing(20)
-
-        for button in self.buttons.values():
-            vbox.addWidget(button)
-            vbox.addSpacing(10)
-
+        vbox.addWidget(self.start_button)
         vbox.addStretch()
+
         self.setLayout(vbox)
 
     def start_programm(self):
-        program = {segment: {parameter: entry.value() for parameter, entry in seg_dict.items()}
-                   for segment, seg_dict in self.entries.items()}
+        if self.start_button.isChecked():
+            program = {segment: {parameter: entry.value() for parameter, entry in seg_dict.items()}
+                       for segment, seg_dict in self.entries.items()}
 
-        pubsub.pub.sendMessage(topicName='gui.set.start_program', program=program)
+            pubsub.pub.sendMessage(topicName='gui.set.start_program', program=program)
+        else:
+            print('STOP!!!')
+            #TODO: Implement stop message to engine
 
     def mark_ramp_segment(self, segment):
         self.radios[segment][0].setChecked(True)
