a632942a9f106e41ebc3a895fcd7d34dcfaf99bb
Alex <alex.schmid@gmail.com>
2023-06-27 12:35:28 +0200

    Adds an about button to the title bar, which displays a dialoge with some info

--- QtInterface/ElchTitleBar.py
+++ QtInterface/ElchTitleBar.py
@@ -1,5 +1,5 @@
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QWidget, QToolButton, QHBoxLayout
+from PySide2.QtWidgets import QWidget, QToolButton, QHBoxLayout, QDialog, QLabel, QVBoxLayout, QPushButton
 
 
 class ElchTitlebar(QWidget):
@@ -8,7 +8,7 @@ class ElchTitlebar(QWidget):
 
         self.setAttribute(Qt.WA_StyledBackground, True)
         self.setMinimumHeight(50)
-        buttons = {key: QToolButton(self, objectName=key) for key in ['Minimize', 'Close']}
+        buttons = {key: QToolButton(self, objectName=key) for key in ['About', 'Minimize', 'Close']}
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
@@ -24,6 +24,7 @@ class ElchTitlebar(QWidget):
         self.dragPosition = None
         buttons['Minimize'].clicked.connect(self.minimize)
         buttons['Close'].clicked.connect(self.close)
+        buttons['About'].clicked.connect(self.show_about)
 
     def mouseMoveEvent(self, event):
         # Enable mouse dragging
@@ -42,3 +43,31 @@ class ElchTitlebar(QWidget):
 
     def close(self):
         self.parent().close()
+
+    def show_about(self):
+        dlg = QDialog(self)
+        dlg.setWindowTitle('About')
+        dlg.setWindowFlags(Qt.FramelessWindowHint | Qt.Dialog)
+
+        vbox = QVBoxLayout()
+        vbox.setContentsMargins(20, 20, 20, 20)
+        vbox.setSpacing(10)
+        vbox.addWidget(QLabel('ElchiTools 2.5.0', objectName='Header'), alignment=Qt.AlignHCenter)
+
+        license_label = QLabel()
+        license_label.setText('License: <a href="https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html">LGPL 2.1</a>')
+        license_label.setOpenExternalLinks(True)
+        vbox.addWidget(license_label, alignment=Qt.AlignHCenter)
+
+        vbox.addWidget(QLabel('Maintainer: Alex Schmid'), alignment=Qt.AlignHCenter)
+        vbox.addWidget(QLabel('Contact: alex.schmid91@gmail.com'), alignment=Qt.AlignHCenter)
+
+        source_label = QLabel()
+        source_label.setText('Source: <a href="https://github.com/AlexSchmid22191/HeaterControl">GitHub</a>')
+        source_label.setOpenExternalLinks(True)
+        vbox.addWidget(source_label, alignment=Qt.AlignHCenter)
+        vbox.addWidget(button := QPushButton('Close'))
+        button.clicked.connect(dlg.close)
+
+        dlg.setLayout(vbox)
+        dlg.exec_()
