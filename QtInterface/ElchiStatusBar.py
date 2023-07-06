b994da45996e510cb018a9d3b37b00c5a06f1ccf
Alex <alex.schmid@gmail.com>
2023-07-06 12:05:50 +0200

    Adds explanatory labels to the status bar.

--- QtInterface/ElchiStatusBar.py
+++ QtInterface/ElchiStatusBar.py
@@ -13,14 +13,14 @@ class ElchStatusBar(QWidget):
 
         parameters = ['Sensor PV', 'Controller PV', 'Setpoint', 'Power']
         icons = {key: QLabel() for key in parameters}
-        labels = {key: QLabel(text=key, objectName='label') for key in parameters}
+        self.labels = {key: QLabel(text=key+'\n ---', objectName='label') for key in parameters}
         self.values = {key: QLabel(text='- - -', objectName='value') for key in parameters}
         vboxes = {key: QVBoxLayout() for key in parameters}
 
         hbox = QHBoxLayout()
         for key in parameters:
             vboxes[key].addWidget(self.values[key])
-            vboxes[key].addWidget(labels[key])
+            vboxes[key].addWidget(self.labels[key])
             vboxes[key].setContentsMargins(0, 0, 0, 0)
             vboxes[key].setSpacing(5)
             hbox.addWidget(icons[key])
@@ -36,6 +36,11 @@ class ElchStatusBar(QWidget):
         self.timer.start(1000)
         pubsub.pub.subscribe(self.update_values, topicName='engine.answer.status')
 
+        self.exp_labels = {'Sensor PV': {'Temperature': 'Sample Temperature', 'Voltage': 'Pump Voltage'},
+                           'Controller PV': {'Temperature': 'Oven Temperature', 'Voltage': '\u03bb Probe Voltage'},
+                           'Setpoint': {'Temperature': 'Target Oven Temperature', 'Voltage': 'Target \u03bb Probe Voltage'},
+                           'Power':{'Temperature': 'Oven Power', 'Voltage': 'Pump Power'}}
+
     def update_values(self, status_values):
         assert isinstance(status_values, dict), 'Illegal data type recieved: {:s}'.format(str(type(status_values)))
 
@@ -47,4 +52,10 @@ class ElchStatusBar(QWidget):
                 self.values[key].setText('{:.1f} {:s}'.format(value[0], self.unit))
 
     def change_units(self, mode):
-        self.unit = {'Temperature': '\u00B0C', 'Voltage': 'mV'}[mode]
+        match mode:
+            case 'Temperature':
+                self.unit = '\u00B0C'
+            case 'Voltage':
+                self.unit = 'mV'
+        for key, label in self.exp_labels.items():
+            self.labels[key].setText(key + '\n' + label[mode])
