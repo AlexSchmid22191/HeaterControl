96333f8ca8836450caf0498bdd64549b02af6d7a
Alex <alex.schmid@gmail.com>
2025-11-25 16:31:58 +0100

    Make the setpoint program menu only have 2 lines at first and buttons to add/remove lines

--- src/Interface/ElchMenu/ElchProgramMenu.py
+++ src/Interface/ElchMenu/ElchProgramMenu.py
@@ -8,14 +8,14 @@ class ElchProgramMenu(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        engine_signals.ramp_segment_started.connect(self.mark_ramp_segment)
+        engine_signals.hold_segment_started.connect(self.mark_hold_segment)
+
         self.labels = {'Rate': QLabel(text='Rate\n(\u00B0C/min)'),
                        'Setpoint': QLabel(text='Setpoint\n(\u00B0C)'),
                        'Hold': QLabel(text='Hold\n(min)')}
-        self.entries = {segment: {'Rate': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=120, value=5),
-                                  'Setpoint': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=1200),
-                                  'Hold': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=10000)
-                                  } for segment in range(1, 11)}
 
+        self.entries = {segment: self.make_entry_row() for segment in range(1, 3)}
         self.radios = {key: {'ramp': QRadioButton(), 'hold': QRadioButton()} for key in self.entries}
 
         radiogroup = QButtonGroup()
@@ -25,30 +25,30 @@ class ElchProgramMenu(QWidget):
                 radiogroup.addButton(radiobutton)
                 radiobutton.setEnabled(False)
 
-        engine_signals.ramp_segment_started.connect(self.mark_ramp_segment)
-        engine_signals.hold_segment_started.connect(self.mark_hold_segment)
-
-        grid = QGridLayout()
-        grid.setSpacing(5)
-        grid.addWidget(self.labels['Rate'], 0, 1)
-        grid.addWidget(self.labels['Setpoint'], 0, 2)
-        grid.addWidget(self.labels['Hold'], 0, 3)
+        self.grid = QGridLayout()
+        self.grid.setSpacing(5)
+        self.grid.addWidget(self.labels['Rate'], 0, 1)
+        self.grid.addWidget(self.labels['Setpoint'], 0, 2)
+        self.grid.addWidget(self.labels['Hold'], 0, 3)
 
         for segment, seg_dict in self.entries.items():
-            grid.addWidget(self.radios[segment]['ramp'], segment, 0)
-            grid.addWidget(seg_dict['Rate'], segment, 1)
-            grid.addWidget(seg_dict['Setpoint'], segment, 2)
-            grid.addWidget(seg_dict['Hold'], segment, 3)
-            grid.addWidget(self.radios[segment]['hold'], segment, 4)
+            self.grid.addWidget(self.radios[segment]['ramp'], segment, 0)
+            self.grid.addWidget(seg_dict['Rate'], segment, 1)
+            self.grid.addWidget(seg_dict['Setpoint'], segment, 2)
+            self.grid.addWidget(seg_dict['Hold'], segment, 3)
+            self.grid.addWidget(self.radios[segment]['hold'], segment, 4)
 
-        self.start_button = QPushButton(text='Start')
+        self.add_button = QPushButton('Add row')
+        self.add_button.clicked.connect(self.add_row)
+        self.remove_button = QPushButton('Remove row')
+        self.remove_button.clicked.connect(self.remove_row)
+
+        self.start_button = QPushButton('Start')
         self.start_button.setObjectName('Start')
         self.start_button.setCheckable(True)
-        # noinspection PyUnresolvedReferences
         self.start_button.toggled.connect(self.start_program)
 
-        self.skip_button = QPushButton(text='Skip segment')
-        # noinspection PyUnresolvedReferences
+        self.skip_button = QPushButton('Skip segment')
         self.skip_button.clicked.connect(self.skip_segment)
 
         vbox = QVBoxLayout()
@@ -58,7 +58,12 @@ class ElchProgramMenu(QWidget):
         vbox.addWidget(l := QLabel(text='Setpoint Programmer'))
         l.setObjectName('Header')
         vbox.addSpacing(10)
-        vbox.addLayout(grid)
+        vbox.addLayout(self.grid)
+        vbox.addStretch(1)
+        vbox.addSpacing(5)
+        vbox.addWidget(self.add_button)
+        vbox.addSpacing(5)
+        vbox.addWidget(self.remove_button)
         vbox.addSpacing(10)
         vbox.addWidget(self.start_button)
         vbox.addSpacing(5)
@@ -66,6 +71,34 @@ class ElchProgramMenu(QWidget):
         vbox.addStretch()
 
         self.setLayout(vbox)
+
+    def add_row(self):
+        n_row = max(self.entries.keys()) + 1
+        self.entries.update({n_row: self.make_entry_row()})
+        self.radios.update({n_row: {'ramp': QRadioButton(), 'hold': QRadioButton()}})
+        self.grid.addWidget(self.radios[n_row]['ramp'], n_row, 0)
+        self.grid.addWidget(self.entries[n_row]['Rate'], n_row, 1)
+        self.grid.addWidget(self.entries[n_row]['Setpoint'], n_row, 2)
+        self.grid.addWidget(self.entries[n_row]['Hold'], n_row, 3)
+        self.grid.addWidget(self.radios[n_row]['hold'], n_row, 4)
+
+    def remove_row(self):
+        n_row = max(self.entries.keys())
+        for widget in self.entries[n_row].values():
+            self.grid.removeWidget(widget)
+            widget.deleteLater()
+        for widget in self.radios[n_row].values():
+            self.grid.removeWidget(widget)
+            widget.deleteLater()
+        self.entries.pop(n_row)
+        self.radios.pop(n_row)
+
+    @staticmethod
+    def make_entry_row():
+        return {'Rate': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=120, value=5),
+                'Setpoint': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=1200),
+                'Hold': QDoubleSpinBox(decimals=1, singleStep=1, minimum=0, maximum=10000)
+                }
 
     def start_program(self):
         if self.start_button.isChecked():
