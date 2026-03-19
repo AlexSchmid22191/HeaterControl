81f56c32bac784abfd22461b1e33262c994d191d
Alex <alex.schmid@gmail.com>
2026-03-19 09:17:08 +0100

    Move kill button in elchi ribbon to the bottom

--- src/Interface/ElchRibbon.py
+++ src/Interface/ElchRibbon.py
@@ -11,26 +11,30 @@ class ElchRibbon(QWidget):
         self.setAttribute(Qt.WA_StyledBackground, True)
         self.menus = menus if menus is not None else ['Devices', 'Control', 'Setpoints', 'PID', 'Plotting', 'Logging']
         self.menu_buttons = {key: QPushButton(parent=self) for key in self.menus}
-        self.menu_buttons.update({'Kill': QPushButton(parent=self)})
         for key, button in self.menu_buttons.items():
             button.setObjectName(key)
         self.buttongroup = QButtonGroup()
         elchi_icon = QLabel()
         elchi_icon.setPixmap(QPixmap('Icons/ElchiHead.png'))
 
+        self.kill_button = QPushButton(parent=self)
+        self.kill_button.setObjectName('Kill')
+        self.kill_button.setFixedSize(150, 100)
+
         vbox = QVBoxLayout()
         vbox.addWidget(elchi_icon, alignment=Qt.AlignHCenter)
         for key in self.menu_buttons:
             vbox.addWidget(self.menu_buttons[key])
-            if key != 'Kill':
-                self.buttongroup.addButton(self.menu_buttons[key])
-                self.menu_buttons[key].setCheckable(True)
+            self.buttongroup.addButton(self.menu_buttons[key])
+            self.menu_buttons[key].setCheckable(True)
             self.menu_buttons[key].setFixedSize(150, 100)
 
         vbox.addStretch()
+        vbox.addWidget(self.kill_button)
+
         vbox.setContentsMargins(0, 0, 0, 0)
         vbox.setSpacing(0)
         self.setMinimumWidth(150)
         self.setLayout(vbox)
 
-        self.menu_buttons['Kill'].clicked.connect(gui_signals.emergency_shutdown.emit)
+        self.kill_button.clicked.connect(gui_signals.emergency_shutdown.emit)
