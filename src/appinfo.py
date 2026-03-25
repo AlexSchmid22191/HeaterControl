31372d4cb979c21e6beb65fb0d651eff20b74a1d
Alex <alex.schmid@gmail.com>
2026-03-25 13:04:06 +0100

    Bump version to 3.3.0.0, update copyright year, and add platformdirs dependency.

--- src/appinfo.py
+++ src/appinfo.py
@@ -1,5 +1,5 @@
 APP_NAME = 'ElchiTools'
-APP_VERSION_NUMERIC = (3, 2, 1, 0)
+APP_VERSION_NUMERIC = (3, 3, 0, 0)
 APP_VERSION = f'{APP_VERSION_NUMERIC[0]}.{APP_VERSION_NUMERIC[1]}'
 
 if APP_VERSION_NUMERIC[2] != 0:
@@ -9,7 +9,7 @@ if APP_VERSION_NUMERIC[2] != 0:
 
 APP_DESCRIPTION = 'A desktop application for interfacing with PID heater and voltage controllers and sensors.'
 APP_AUTHOR = 'Alex Schmid'
-APP_COPYRIGHT = '© 2025 Alex Schmid'
+APP_COPYRIGHT = '© 2026 Alex Schmid'
 APP_COMPANY = 'TU Wien'
 APP_SOURCE = 'https://github.com/AlexSchmid22191/HeaterControl'
 APP_CONTACT = 'alex.schmid91@gmail.com'
